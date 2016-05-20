b41c1066adfaa52ff719670c3801a7a2b473fc1b
Theo Gregory <tjmg@outlook.com>
2016-05-20 18:01:23 +0100

    Allows file naming of imaging, and adds the font used.

--- testing/imaging.py
+++ testing/imaging.py
@@ -1,9 +1,9 @@
 from PIL import Image, ImageFont, ImageDraw
 
-def draw(string):
+def draw(content, fileName):
 	image = Image.new("RGBA", (1056,704), (255,255,255))
 	draw = ImageDraw.Draw(image)
 	font = ImageFont.truetype("courier-new.ttf", 100)
-	draw.text((10, 0), string, (0,0,0), font=font)
-	img_resized = image.resize((264,176), Image.ANTIALIAS)
-	image.save('sample-out2.jpg')
+	draw.text((10, 0), content, (0,0,0), font=font)
+	# img_resized = image.resize((264,176), Image.ANTIALIAS)
+	image.save(fileName+'.jpg')
